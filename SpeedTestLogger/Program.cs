cff29be23c88903720210a5ad5faa22caab76fa0
Simen Samuelsen <sisa@cxmac-00429.local>
2018-11-03 11:34:40 +0100

    First speedlogger

---
+++ SpeedTestLogger/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using SpeedTest;
+using SpeedTest.Models;
+using SpeedTestLogger.Models;
+
+namespace SpeedTestLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SpeedTestLogger!");
+
+            var config = new LoggerConfiguration();
+            var runner = new SpeedTestRunner(config.LoggerLocation);
+            var testData = runner.RunSpeedTest();
+            var results = new TestResult
+            {
+                SessionId = new Guid(),
+                User = config.UserId,
+                Device = config.LoggerId,
+                Timestamp = DateTimeOffset.Now.ToUnixTimeMilliseconds(),
+                Data = testData
+            };
+        }
+    }
+}
